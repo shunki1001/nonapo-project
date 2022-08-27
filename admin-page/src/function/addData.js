838f5ad4ebcc494dc9349c070bb986b89be80544
shunki <fateveriki@gmail.com>
2022-08-27 13:49:57 +0900

    ユーザー作成時に自動でアカウント作成

--- admin-page/src/function/addData.js
+++ admin-page/src/function/addData.js
@@ -14,8 +14,69 @@ const addData = async (data) => {
     numberOfAccount: data.numberOfAccount,
     subscriptionCost: data.subscriptionCost,
     status: data.isAgreement,
+    isAgreement: data.isAgreement,
   });
+  localStorage.setItem("tempId", docRef.id);
+  // localStorage.setItem("company", docRef.data().enterprise);
 
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  for (let i = 0; i < data.numberOfAccount; i++) {
+    try {
+      const docRef = await addDoc(collection(db, "account"), {
+        username: `ユーザー${i + 1}`,
+        email: "",
+        phone: "",
+        url: "",
+        isGoogleCalendar: true,
+        dayOfWeekChoices: {
+          mon: true,
+          tue: true,
+          wed: true,
+          thu: true,
+          fri: true,
+          sat: false,
+          sun: false,
+        },
+        startTime: "",
+        endTime: "",
+        company: localStorage.getItem("company"),
+        isOneSubButton: true,
+        mainButton: "",
+        googleId: "",
+        mailSubject: "",
+        mailContent: "",
+        enterprise: localStorage.getItem("tempId"),
+      });
+      const userId = docRef.id;
+      if (i === 0) {
+        try {
+          await addDoc(collection(db, "account", userId, "button"), {
+            isOnly: true,
+            title: "",
+            url: "",
+          });
+        } catch (error) {
+          console.log("アカウントのボタンコレクションでエラー");
+          console.log(error);
+        }
+      } else {
+        try {
+          await addDoc(collection(db, "account", userId, "button"), {
+            isOnly: false,
+            title: "",
+            url: "",
+          });
+        } catch (error) {
+          console.log("アカウントのボタンコレクションでエラー");
+          console.log(error);
+        }
+      }
+    } catch (error) {
+      console.log("アカウントの登録でエラー");
+      console.log(error);
+    }
+  }
   return docRef;
 };
 
